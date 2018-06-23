813abfb8d751e469a121d9751d98ac422553af72
Lucas Stoque <stoque@live.com>
2018-06-22 23:50:40 -0300

    remove navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 import Avatar from '../components/avatar'
 import SocialList from '../components/social-list'
-import Navigation from '../components/navigation'
 
 const IndexPage = () => (
   <div>
     <Avatar/>
     <SocialList/>
-    <Navigation/>
   </div>
 )
 
