745944e0d52e200625d2b405b778bdba95bdebf3
Lucas Stoque <lucas.stoque@corp.globo.com>
2020-03-19 18:42:23 -0300

    fix: og and title

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,7 +23,7 @@ const SEO = ({ description, lang, meta, keywords }) => {
               },
               {
                 property: `og:title`,
-                content: data.site.siteMetadata.titl,
+                content: data.site.siteMetadata.title,
               },
               {
                 property: `og:description`,
@@ -43,7 +43,7 @@ const SEO = ({ description, lang, meta, keywords }) => {
               },
               {
                 name: `twitter:title`,
-                content: data.site.siteMetadata.titl,
+                content: data.site.siteMetadata.title,
               },
               {
                 name: `twitter:description`,
